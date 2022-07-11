a56b7fbdc5e8944d2ce1beb475df819ba743931b
wh <wh@whs-MacBook-Pro.local>
2022-07-11 15:38:17 +0800

    phone

--- produce_phone.py
+++ produce_phone.py
@@ -1,5 +1,5 @@
 with open('/Users/wh/Downloads/phone.txt', 'w') as f:    
-	for i in range(1,100000):
+	for i in range(90000,1900000):
 		phone = '628' + str(i).zfill(10)
 		f.write(phone)
 		f.write('\r')
